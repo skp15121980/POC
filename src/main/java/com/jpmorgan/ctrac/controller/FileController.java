2cb2697fc232cc69189b6002526ab8cf5d361bbe
Sunil Kumar Pandey <skp4java@gmail.com>
2016-10-24 00:36:15 +0530

    Refactered

--- src/main/java/com/jpmorgan/ctrac/controller/FileController.java
+++ src/main/java/com/jpmorgan/ctrac/controller/FileController.java
@@ -16,32 +16,31 @@ import com.jpmorgan.ctrac.util.FileUtil;
 @Controller
 // @RequestMapping("/")
 public class FileController {
-	
+
 	private static final String SOURCE_FILE_PATH = "C:/sourceFolder/";
 	private static final String DESTINATION_FILE_PATH = "D:/destinationFolder/";
 
 	@RequestMapping(value = "/fileList", method = RequestMethod.GET)
 	public String getFiles(/* @PathVariable String name, */ ModelMap model) {
 		model.addAttribute("fileList", FileUtil.fetchAllFiles(FileUtil.getPath(SOURCE_FILE_PATH).getAbsolutePath()));
-		return "hello";
+		return "fileDetails";
 	}
 
 	@RequestMapping(value = "/moveFile", method = RequestMethod.POST)
 	public String moveFile(/* @PathVariable String name, */ ModelMap model) {
 
 		Path movefrom = FileSystems.getDefault().getPath(FileUtil.getPath(SOURCE_FILE_PATH).getAbsolutePath());
-		Path target = FileSystems.getDefault().getPath(DESTINATION_FILE_PATH+"/"+FileUtil.getPath(SOURCE_FILE_PATH).getName());
+		Path target = FileSystems.getDefault()
+				.getPath(DESTINATION_FILE_PATH + "/" + FileUtil.getPath(SOURCE_FILE_PATH).getName());
 		try {
 			Path path = Files.move(movefrom, target, StandardCopyOption.REPLACE_EXISTING);
-			//Path path=Files.copy(movefrom, target);
+			// Path path=Files.copy(movefrom, target);
 			model.addAttribute("message", path.getFileName() + "   Files moved Successfully");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			model.addAttribute("message", "Files not moved Successfully"+e.getMessage());
+			model.addAttribute("message", "Files not moved Successfully" + e.getMessage());
 		}
-		return "result";
+		return "message";
 	}
-
-	
 }
